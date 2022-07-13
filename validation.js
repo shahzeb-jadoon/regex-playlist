855d342e6f235d999ba8dc95a5162b9348f6e8fb
Shahzeb Jadoon <shahzeb.jadoon.sj@gmail.com>
2022-07-13 00:27:36 -0500

    Finalize RegEx form

--- validation.js
+++ validation.js
@@ -1,4 +1,27 @@
 // validation script here
+const inputs = document.querySelectorAll('input');
+
 const patterns = {
-    telephone: /^\d{10}$/
+    username: /^[a-z\d]{5,12}$/i,
+    email: /^([a-z\d-\.]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+    password: /^[\w@-]{8,20}$/,
+    telephone: /^\d{10}$/,
+    slug: /^[a-z\d-]{8,20}$/
 };
+
+//validation function
+function validate(field, regex){
+    //console.log(regex.test(field.value));
+    if (regex.test(field.value)) {
+        field.className = 'valid';
+    } else {
+        field.className = 'invalid';
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        //console.log(e.target.attributes.name.value);
+        validate(e.target, patterns[e.target.attributes.name.value]);
+    });
+});
